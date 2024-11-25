b04bf261c947f523ca70c8448902884dbc7b1373
pchchv <ipchchv@gmail.com>
2024-11-25 14:03:04 +0300

    Implement a ReadFull method

--- source.go
+++ source.go
@@ -26,3 +26,27 @@ func (s *source) Seek(position int64, whence int) (n int64, err error) {
 
 	return n, nil
 }
+
+func (s *source) ReadFull(buf []byte) (n int, err error) {
+	var read int
+	if s.buf != nil {
+		read = copy(buf, s.buf)
+		if len(s.buf) > read {
+			s.buf = s.buf[read:]
+		} else {
+			s.buf = nil
+		}
+
+		if len(buf) == read {
+			return read, nil
+		}
+	}
+
+	if n, err = io.ReadFull(s.reader, buf[read:]); err == io.ErrUnexpectedEOF {
+		// allow if all data can't be read
+		err = io.EOF
+	}
+
+	s.pos += int64(n)
+	return n + read, err
+}
