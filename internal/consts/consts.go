f623cea7362105f4911b3eb0517ce8717d0d06d7
pchchv <ipchchv@gmail.com>
2024-11-25 14:14:51 +0300

    Create Version and Layer types

---
+++ internal/consts/consts.go
@@ -0,0 +1,17 @@
+package consts
+
+const (
+	Version2_5      Version = 0
+	VersionReserved Version = 1
+	Version2        Version = 2
+	Version1        Version = 3
+
+	LayerReserved Layer = 0
+	Layer3        Layer = 1
+	Layer2        Layer = 2
+	Layer1        Layer = 3
+)
+
+type Version int
+
+type Layer int
