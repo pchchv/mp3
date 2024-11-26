4c7e30cf6bbac172ddd1bfeea3d33bbc08f7e61a
pchchv <ipchchv@gmail.com>
2024-11-26 15:26:38 +0300

    Implement an initSlen functions

--- internal/maindata/maindata.go
+++ internal/maindata/maindata.go
@@ -9,3 +9,34 @@ type MainData struct {
 	ScalefacS [2][2][13][3]int   // 0-4 bits
 	Is        [2][2][576]float32 // Huffman coded freq. lines
 }
+
+func initSlen() (nSlen2 [512]int) {
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 3; j++ {
+			n := j + i*3
+			nSlen2[n+500] = i | (j << 3) | (2 << 12) | (1 << 15)
+		}
+	}
+
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			for k := 0; k < 4; k++ {
+				for l := 0; l < 4; l++ {
+					n := l + k*4 + j*16 + i*80
+					nSlen2[n] = i | (j << 3) | (k << 6) | (l << 9) | (0 << 12)
+				}
+			}
+		}
+	}
+
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			for k := 0; k < 4; k++ {
+				n := k + j*4 + i*20
+				nSlen2[n+400] = i | (j << 3) | (k << 6) | (1 << 12)
+			}
+		}
+	}
+
+	return
+}
