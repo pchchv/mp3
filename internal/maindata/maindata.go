f59a8ba7d8e1a1546d940358de48b85a37321bda
pchchv <ipchchv@gmail.com>
2024-11-26 15:25:28 +0300

    Create type MainData

--- internal/maindata/maindata.go
+++ internal/maindata/maindata.go
@@ -2,3 +2,10 @@
 type FullReader interface {
 	ReadFull([]byte) (int, error)
 }
+
+// MainData is MPEG1 Layer 3 Main Data.
+type MainData struct {
+	ScalefacL [2][2][22]int      // 0-4 bits
+	ScalefacS [2][2][13][3]int   // 0-4 bits
+	Is        [2][2][576]float32 // Huffman coded freq. lines
+}
