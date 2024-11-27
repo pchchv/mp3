835bfd2c4f95fa5e4bf89eb185cd0eb1584ce9f5
pchchv <ipchchv@gmail.com>
2024-11-27 11:57:14 +0300

    Implement a getScaleFactorsMpeg2 func

--- internal/maindata/maindata.go
+++ internal/maindata/maindata.go
@@ -1,4 +1,15 @@
 
+var (
+	nSlen2             = initSlen() /* MPEG 2.0 slen for 'normal' mode */
+	scalefacSizesMpeg2 = [3][6][4]int{
+		{{6, 5, 5, 5}, {6, 5, 7, 3}, {11, 10, 0, 0},
+			{7, 7, 7, 0}, {6, 6, 6, 3}, {8, 8, 5, 0}},
+		{{9, 9, 9, 9}, {9, 9, 12, 6}, {18, 18, 0, 0},
+			{12, 12, 12, 0}, {12, 9, 9, 6}, {15, 12, 9, 0}},
+		{{6, 9, 9, 9}, {6, 9, 12, 6}, {15, 18, 0, 0},
+			{6, 15, 12, 0}, {6, 12, 9, 6}, {6, 18, 9, 0}}}
+)
+
 type FullReader interface {
 	ReadFull([]byte) (int, error)
 }
@@ -39,4 +50,64 @@ func initSlen() (nSlen2 [512]int) {
 	}
 
 	return
+}
+
+func getScaleFactorsMpeg2(m *bits.Bits, header frameheader.FrameHeader, sideInfo *sideinfo.SideInfo) (*MainData, *bits.Bits, error) {
+	nch := header.NumberOfChannels()
+	md := &MainData{}
+	for ch := 0; ch < nch; ch++ {
+		part_2_start := m.BitPos()
+		numbits := 0
+		slen := nSlen2[sideInfo.ScalefacCompress[0][ch]]
+		sideInfo.Preflag[0][ch] = (slen >> 15) & 0x1
+
+		n := 0
+		if sideInfo.BlockType[0][ch] == 2 {
+			n++
+			if sideInfo.MixedBlockFlag[0][ch] != 0 {
+				n++
+			}
+		}
+
+		var scaleFactors []int
+		d := (slen >> 12) & 0x7
+		for i := 0; i < 4; i++ {
+			num := slen & 0x7
+			slen >>= 3
+			if num > 0 {
+				for j := 0; j < scalefacSizesMpeg2[n][d][i]; j++ {
+					scaleFactors = append(scaleFactors, m.Bits(num))
+				}
+				numbits += scalefacSizesMpeg2[n][d][i] * num
+			} else {
+				for j := 0; j < scalefacSizesMpeg2[n][d][i]; j++ {
+					scaleFactors = append(scaleFactors, 0)
+				}
+			}
+		}
+
+		n = (n << 1) + 1
+		for i := 0; i < n; i++ {
+			scaleFactors = append(scaleFactors, 0)
+		}
+
+		if len(scaleFactors) == 22 {
+			for i := 0; i < 22; i++ {
+				md.ScalefacL[0][ch][i] = scaleFactors[i]
+			}
+		} else {
+			for x := 0; x < 13; x++ {
+				for i := 0; i < 3; i++ {
+					md.ScalefacS[0][ch][x][i] = scaleFactors[(x*3)+i]
+				}
+			}
+		}
+
+		// read Huffman coded data. Skip stuffing bits
+		if err := readHuffman(m, header, sideInfo, md, part_2_start, 0, ch); err != nil {
+			return nil, nil, err
+		}
+	}
+	// ancillary data is stored here,but we ignore it
+	return md, m, nil
 }
