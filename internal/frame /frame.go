43adaa36e00087af7c1299eb9d4702eecf37e0ed
pchchv <ipchchv@gmail.com>
2024-11-27 12:29:46 +0300

    Implement a method reorder

--- internal/frame /frame.go
+++ internal/frame /frame.go
@@ -17,6 +17,57 @@ type Frame struct {
 	v_vec        [2][1024]float32
 }
 
+func (f *Frame) reorder(gr int, ch int) {
+	re := make([]float32, consts.SamplesPerGr)
+	_, sfBandIndicesShort := getSfBandIndicesArray(&f.header)
+	// only reorder short blocks
+	if (f.sideInfo.WinSwitchFlag[gr][ch] == 1) && (f.sideInfo.BlockType[gr][ch] == 2) { // Short blocks
+		// check if the first two subbands
+		// (=2*18 samples = 8 long or 3 short sfb's) uses long blocks
+		sfb := 0
+		// 2 longbl
+		// sb first
+		if f.sideInfo.MixedBlockFlag[gr][ch] != 0 {
+			sfb = 3
+		}
+
+		next_sfb := sfBandIndicesShort[sfb+1] * 3
+		win_len := sfBandIndicesShort[sfb+1] - sfBandIndicesShort[sfb]
+		i := 36
+		if sfb == 0 {
+			i = 0
+		}
+
+		for i < consts.SamplesPerGr {
+			// check if we're into the next scalefac band
+			if i == next_sfb {
+				// copy reordered data back to the original vector
+				j := 3 * sfBandIndicesShort[sfb]
+				copy(f.mainData.Is[gr][ch][j:j+3*win_len], re[0:3*win_len])
+				// check if this band is above the rzero region,if so we're done
+				if i >= f.sideInfo.Count1[gr][ch] {
+					return
+				}
+
+				sfb++
+				next_sfb = sfBandIndicesShort[sfb+1] * 3
+				win_len = sfBandIndicesShort[sfb+1] - sfBandIndicesShort[sfb]
+			}
+
+			for win := 0; win < 3; win++ { // Do the actual reordering
+				for j := 0; j < win_len; j++ {
+					re[j*3+win] = f.mainData.Is[gr][ch][i]
+					i++
+				}
+			}
+		}
+
+		// copy reordered data of last band back to original vector
+		j := 3 * sfBandIndicesShort[12]
+		copy(f.mainData.Is[gr][ch][j:j+3*win_len], re[0:3*win_len])
+	}
+}
+
 func getSfBandIndicesArray(header *frameheader.FrameHeader) ([]int, []int) {
 	sfreq := header.SamplingFrequency() // Setup sampling frequency index
 	lsf := header.LowSamplingFrequency()
